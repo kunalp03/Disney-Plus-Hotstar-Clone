edf854be4f9a4fc4789ce4f128686c17465f9a5b
Kunal Patil <kp327313@gmail.com>
2022-08-01 20:23:48 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,126 @@
+import React from 'react'
+import styled from 'styled-components'
+import MenuIcon from '@mui/icons-material/Menu';
+
+function Header() {
+    function increaseWidth(e){
+        // e.target.styled.input.wid 
+    }
+    return (
+        <>
+            <Nav>
+                <CustomMenu />
+                <Logo src="/images/disney-logo.png" />
+                <NavMenu>
+                    <ul>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">Sports</a></li>
+                        <li><a href="#">Disney+</a></li>
+                        <li><a href="#">KIDS</a></li>
+                    </ul>
+                </NavMenu>
+                <RightNav>
+                    <input placeholder="Search" onClick={increaseWidth}></input>
+                    <button>Subscribe</button>
+                    <div className="btn">Login</div>
+                </RightNav>
+            </Nav>
+        </>
+    )
+}
+
+export default Header
+
+const Nav = styled.nav`
+        overflow-x: hidden;
+            height: 70px;
+            background: linear-gradient(to bottom, #141b29, #0c111b 300px);
+            display:flex;
+            align-items: center;
+            padding: 0 70px;
+            font-family: "Roboto","HelveticaNeue-Light",sans-serif;
+`
+const CustomMenu = styled(MenuIcon)`
+            cursor: pointer;
+            color: white;
+`
+const Logo = styled.img`
+            width: 115px;
+            margin-bottom: 7px;
+            padding: 0 30px;
+`
+const NavMenu = styled.div`
+    flex: 1;
+    ul{
+        display: flex;
+    }
+    li{
+        list-style: none;
+    }
+    a{
+        padding-right: 40px;
+        color: rgba(255, 255, 255, 0.8);
+        text-decoration: none;
+        position: relative;
+        &:after{
+            content: "";
+            height: 2px;
+            margin-right: 34px;
+            background: white;
+            position: absolute;
+            left: 0;
+            right: 0;
+            bottom: -6px;
+            opacity: 0;
+            transform-origin: left center;
+            transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+            transform: scaleX(0);
+        }
+        &:hover{
+            &:after{
+                transform: scaleX(0.9);
+                opacity: 1;
+            }
+        }
+    }
+    
+`
+const RightNav = styled.div`
+    .btn{
+        font-weight: 500;
+        text-transform: uppercase;
+        color: rgba(255, 255, 255, 0.8);
+        text-decoration: none;
+        cursor: pointer;
+        display: inline-block;
+        padding: 13px 0;
+        margin-left: 15px;
+        opacity: 1;
+        font-size: 14px;
+    }
+
+    input{
+        background: transparent;
+        border: none;
+        border-bottom: 2px solid rgba(255, 255, 255, 0.6);;
+        width: 240px;
+        outline: none;
+        ::placeholder {
+            color: rgba(255, 255, 255, 0.8);
+            font-size: 15px;
+        }
+    }
+    button{
+        background-color: #1f80e0;
+        cursor: pointer;
+        border-radius: 4px;
+        text-transform: uppercase;
+        font-weight: 700;
+        letter-spacing: 0.4px;
+        padding: 2px 10px;
+        margin-left: 22px;
+        font-size: 12px;
+        line-height: 19px;
+    }            
+`
---
+++ src/components/ImageSlider.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import styled from 'styled-components';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import Slider from 'react-slick'
+
+function ImageSlider() {
+    let settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true
+      };
+    return (
+        <Container>
+            <Carousel {...settings}>
+                <Wrap>
+                    <img src="/images/slider-badging.jpg"/>
+                </Wrap>
+                <Wrap>
+                    <img src="/images/slider-badag.jpg"/>
+                </Wrap>
+            </Carousel>
+        </Container>
+    )
+}
+
+export default ImageSlider
+
+const Container = styled.div`
+    width: 100vw;
+    // overflow-x: hidden;
+    // overflow-y: hidden;
+`;
+
+const Carousel = styled(Slider) `
+    margin-top: 20px;
+    width: 90%;
+    .slick-list{
+        overflow: visible;
+    }
+    .slick-next{
+        right: -100px;
+    }
+    .slick-prev{
+        left: 50px;
+        z-index: 10;
+    }
+    ul li button{
+        &:before{
+            font-size: 10px;
+            color: rgb(150, 158, 171);
+        }
+    }
+    li.slick-active button:before{
+        color:white;
+    }
+`
+const Wrap = styled.div `
+    cursor: pointer;
+    // margin: 100px;
+    img{
+        align-self: center;
+        border-radius: 4px;
+        width: 100%;
+        height: 100%;
+        box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px, rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+        margin-left: 5vw;
+    }
+`
